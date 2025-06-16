511015e71b67f46fc1ba40d18c018f710e497642
RicardoCiotto <ryukyzp@gmail.com>
2025-06-15 22:08:53 -0300

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   get "home", controller: "home", action: "index"
+  get "estudantes", to: "estudante#index"
+
+  root "home#index"
 
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
